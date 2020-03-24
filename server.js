f193f249bff87e2814e360e3b08aaa38c22e9dbf
alecharl <ale-charl@hotmail.com>
2020-03-24 10:23:57 -0300

    express.router añadido

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ router.post('/message', function(req, res){
 //   res.send ('Hola');
 //});
 
-app.listen(3001);
-console.log ('La aplicacion esta escuchando en http://localhost:3001');
+app.listen(3000);
+console.log ('La aplicacion esta escuchando en http://localhost:3000');
