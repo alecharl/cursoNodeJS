fd654b0508fb287897ebde2fd67f7ee6bc385d24
alecharl <ale-charl@hotmail.com>
2020-03-24 02:33:51 -0300

    saludos desde GET y POST

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require ('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req, res){
-    res.send ('Hola');
+app.use(router);
+
+router.get('/', function(req, res){
+    res.send ('hola desde GET');
 });
 
-app.listen(3000);
-console.log ('La aplicacion esta escuchando en http://localhost:3000');
+router.post('/', function(req, res){
+    res.send ('hola desde POST');
+});
+
+//app.use('/', function (req, res){
+//   res.send ('Hola');
+//});
+
+app.listen(3001);
+console.log ('La aplicacion esta escuchando en http://localhost:3001');
